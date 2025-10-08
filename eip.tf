e48328eeae40c8e5e5e15de731c0279f7e71fd97
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-10-08 23:58:56 +0200

    test

--- eip.tf
+++ eip.tf
@@ -1,5 +1,5 @@
-resource "aws_eip" "bastion-eip" {
+/*resource "aws_eip" "bastion-eip" {
   depends_on = [aws_instance.ec2-vm-public, module.vpc]
   instance   = aws_instance.ec2-vm-public.id
   tags = local.common_tags
-}
+}*/
