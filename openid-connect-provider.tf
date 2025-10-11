34303bd4ab401e57a3bc7d959b6dfb020f6aa9d2
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-10-11 02:28:58 +0200

    adding new files

--- openid-connect-provider.tf
+++ openid-connect-provider.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_openid_connect_provider" "oidc_provider" {
+/*resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
   thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
   url             = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
@@ -10,3 +10,4 @@ resource "aws_iam_openid_connect_provider" "oidc_provider" {
     local.common_tags
   )
 }
+*/
