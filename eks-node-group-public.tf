437a15e06a5e507f1af3570fe29d237de1166bb7
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-10-13 11:18:16 +0200

    add

--- eks-node-group-public.tf
+++ eks-node-group-public.tf
@@ -1,6 +1,6 @@
 # Create AWS EKS Node Group - Public
-resource "aws_eks_node_group" "eks_ng_public" {
-  #cluster_name = aws_eks_cluster.eks-cluster.name
+/*resource "aws_eks_node_group" "eks_ng_public" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
 
   node_group_name = "${local.Name}-eks-ng-public"
   node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
@@ -41,3 +41,5 @@ resource "aws_eks_node_group" "eks_ng_public" {
     Name = "Public-Node-Group"
   }
 }
+
+*/
